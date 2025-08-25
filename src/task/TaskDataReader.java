c2e6f8b8046f0c93b83e9881f9ef3a93c6adabec
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2025-08-25 13:45:44 +0300

    feat: change readAsync return type to CompletableFuture

--- src/task/TaskDataReader.java
+++ src/task/TaskDataReader.java
@@ -7,22 +7,32 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TaskDataReader {
+    private static final Logger logger = Logger.getLogger(TaskDataReader.class.getName());
     private final Executor executor;
 
     public TaskDataReader(ExecutorService executor) {
         this.executor = Objects.requireNonNull(executor);
     }
 
-    public List<TaskData> readAsync(List<String> filePaths) {
+    public CompletableFuture<List<TaskData>> readAsync(List<String> filePaths) {
         var futures = filePaths.stream()
-                .map(path -> CompletableFuture.supplyAsync(() -> readFile(path), executor))
+                .map(path -> CompletableFuture.supplyAsync(() -> readFile(path), executor)
+                        .exceptionally(e -> {
+                            logger.log(Level.SEVERE,"Failed to read file: " + path, e);
+
+                            return null;
+                        }))
                 .toList();
 
-        return futures.stream()
-                .map(CompletableFuture::join)
-                .toList();
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(_ -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .filter(Objects::nonNull)
+                        .toList());
     }
 
     private TaskData readFile(String filePath) {
